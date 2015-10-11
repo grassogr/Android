85e0efb36ea6bdcd2f790f850ac4794f6c642f8e
Grassogr <grassogr@gmail.com>
2015-10-11 17:49:15 +0300

    1st commit
    1st commit

---
+++ autoanswer/app/src/main/java/com/gv/www/autoanswer/R.java
@@ -0,0 +1,48 @@
+package com.gv.www.autoanswer;
+
+public final class R
+{
+  public static final class array
+  {
+    public static final int delay = 2130968578;
+    public static final int delay_values = 2130968579;
+    public static final int which_contacts = 2130968576;
+    public static final int which_contacts_values = 2130968577;
+  }
+  
+  public static final class attr {}
+  
+  public static final class drawable
+  {
+    public static final int icon = 2130837504;
+    public static final int stat_sys_autoanswer = 2130837505;
+  }
+  
+  public static final class string
+  {
+    public static final int about_message = 2131034120;
+    public static final int about_title = 2131034119;
+    public static final int app_name = 2131034118;
+    public static final int delay = 2131034114;
+    public static final int enabled = 2131034112;
+    public static final int headset_only = 2131034116;
+    public static final int license_message = 2131034122;
+    public static final int license_title = 2131034121;
+    public static final int no_second_call = 2131034117;
+    public static final int notification_text = 2131034124;
+    public static final int notification_title = 2131034123;
+    public static final int use_speakerphone = 2131034115;
+    public static final int which_contacts = 2131034113;
+  }
+  
+  public static final class xml
+  {
+    public static final int preferences = 2130903040;
+  }
+}
+
+
+/* Location:              C:\dex2jar\output_jar.jar!\com\everysoft\autoanswer\R.class
+ * Java compiler version: 6 (50.0)
+ * JD-Core Version:       0.7.1
+ */
